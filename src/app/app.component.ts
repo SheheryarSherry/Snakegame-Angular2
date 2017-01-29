eebdc13f49096764943e2735ec60cb81b28d6a8f
SheheryarSherry <sheheryarkhan1992@hotmail.com>
2017-01-29 15:08:08 +0500

    initialized variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,9 +11,49 @@ import { BestScoreManager } from './app.storage.service'
 })
 export class AppComponent implements OnInit {
 	constructor(
-
+		private bestScoreService: BestScoreManager
 	) { }
+	public best_score=this.bestScoreService.retrieve();
 
+	public BOARD_SIZE=18;
+	public board =[];
+	public interval:any;
+	public tempDirection:any;
+	public isGameOver:any;
+	public gameStarted=false;
+	public newBestScore=false;
+	private score=0;
+
+	public CONTROLS ={
+		LEFT:37,
+		UP:38,
+		RIGHT: 39,
+		DOWN:40
+
+	};
+
+	public COLORS={
+		GAME_OVER:'#D24D57',
+		FRUIT:'#EC644B',
+		HEAD: '#336E7B',
+		BODY: '#C8F7C5',
+		BOARD: '#86B5BD'
+	};
+
+	private snake ={
+		direction: this.CONTROLS.LEFT,
+		parts:[
+			{
+				x:-1,
+				y:-1
+			}
+		]
+	};
+
+	private fruit={
+		x: -1,
+		y:-1
+	};
 	
 
 	ngOnInit() {
